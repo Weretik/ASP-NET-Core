d764bb77d1273b097282322910d78ad143278f91
Weretik <vitaliythupin@gmail.com>
2025-03-12 15:22:23 +0200

    Добавьте файлы проекта.

---
+++ Introduction MVC Pattern and Middleware/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Introduction_MVC_Pattern_and_Middleware.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
